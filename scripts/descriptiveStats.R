8196535b37c5c9829a6284872d342060a472ee95
Ellyn Butler <ellynrbutler@gmail.com>
2020-05-19 22:29:49 -0400

    First batch of descriptive plots finalized

--- scripts/descriptiveStats.R
+++ scripts/descriptiveStats.R
@@ -2,18 +2,33 @@
 ### in proposed predictors. It also outputs a cleaned data.frame.
 ###
 ### Ellyn Butler
-### May 7, 2020 - May 18, 2020
+### May 7, 2020 - May 19, 2020
 
 # Load libraries
 library('dplyr')
+library('ggcorrplot')
+library('ggplot2')
+library('ggpubr')
 
 # Load data
 full_df <- read.csv("~/Documents/traumaCOVID/data/ACE_dataset_pulled_05062020.csv")
 full_df$sex <- recode(full_df$sex, "1"="Female", "2"="Male", .default=NA_character_)
 
 # Recode job labels
+full_df$occu_health <- recode(full_df$occupation, "2"="0", "3"="0", "4"="0",
+  "5"="0", "6"="1", "9"="0", "10"="0", "11"="0", "12"="0", "13"="0",
+  "14"="0", "15"="0", "16"="0", "17"="0", "18"="0", "19"="0", "20"="0",
+  "1"="0", "7"="0", "21"="0", "22"="0", "8"="0", .default=NA_character_)
+full_df$occu_health <- as.numeric(full_df$occu_health)
 
-#(Probably):
+full_df$job_reduced <- recode(full_df$exp_job_loss, "0"="0", "4"="1", "2"="1",
+  "1"="0", "3"="1", .default=NA_character_)
+full_df <- full_df[!is.na(full_df$job_reduced),]
+row.names(full_df) <- 1:nrow(full_df)
+full_df$job_reduced <- as.numeric(full_df$job_reduced)
+
+full_df$relationship_last <- full_df$pr_2
+
 # !!! ACE_2 (physical assault): Did a parent or other adult in the household often;  Push, grab, slap, or throw something at you?  or  Ever hit you so hard that you had marks or were injured?
 # !!! ACE_3 (sexual assault): Did an adult or person at least 5 years older than you ever;  Touch or fondle you or have you touch their body in a sexual way?  or  Try to or actually have oral, anal, or vaginal sex with you?
 # !!! ACE_5 (material deprivation): Did you often feel that; You didn't have enough to eat, had to wear dirty clothes, and had no one to protect you? or  Your parents were too drunk or high to take care of you or take you to the doctor if you needed it?
@@ -24,7 +39,9 @@ full_df$sex <- recode(full_df$sex, "1"="Female", "2"="Male", .default=NA_charact
 
 ace_df <- full_df[full_df$ACE_2 %in% c(0,1) & full_df$ACE_3 %in% c(0,1) &
   full_df$ACE_5 %in% c(0,1) & full_df$ACE_7 %in% c(0,1), c("randomID", "age", "sex",
-  "race", "occupation", paste0("GAD_", 1:7), paste0("ISI_", 1:7), paste0("ACE_", 1:10))]
+  "race", "relationship_last", "job_reduced", "occu_health", paste0("GAD_", 1:7),
+  paste0("ISI_", 1:7), paste0("ACE_", 1:10))]
+row.names(ace_df) <- 1:nrow(ace_df)
 
 # ------------------- Co-occurrence of ACEs ------------------- #
 
@@ -57,11 +74,40 @@ aceSummary <- function(ace_df) {
 # Before excluding healthcare workers
 ace_summary <- aceSummary(ace_df)
 
-# After excluding healthcare workers
-ace_nohealth_df <- ace_df[
-ace_summary_nohealth <- aceSummary(
+# After excluding healthcare workers -> Will definitely need to keep
+ace_nohealth_df <- ace_df[ace_df$occu_health == 0 & !is.na(ace_df$occu_health),]
+row.names(ace_nohealth_df) <- 1:nrow(ace_nohealth_df)
+ace_summary_nohealth <- aceSummary(ace_nohealth_df)
 
+# Exclude subjects from ace_df without occupation information
+ace_df <- ace_df[!is.na(ace_df$occu_health),]
+row.names(ace_df) <- 1:nrow(ace_df)
 
+# Plots
+n_df <- data.frame(Adversity=rep(c("physical", "sexual", "witness", "material"), 2),
+  Happened=c(rep("Yes", 4), rep("No", 4)), N=c(nrow(ace_df[ace_df$physical == 1,]),
+  nrow(ace_df[ace_df$sexual == 1,]), nrow(ace_df[ace_df$witness == 1,]),
+  nrow(ace_df[ace_df$material == 1,]), nrow(ace_df[ace_df$physical == 0,]),
+  nrow(ace_df[ace_df$sexual == 0,]), nrow(ace_df[ace_df$witness == 0,]),
+  nrow(ace_df[ace_df$material == 0,])))
+n_plot <- ggplot(n_df, aes(x=Happened, y=N)) + theme_linedraw() +
+  geom_bar(stat="identity") + facet_grid(. ~ Adversity)
 
+bar_plot <- ggplot(ace_df, aes(relationship_last)) + theme_linedraw() +
+  geom_bar() + ggtitle("Relationship will last no matter what?") + xlab("Response")
 
 ##################### IVs correlation table #####################
+
+corr <- round(cor(ace_df[, c("relationship_last", "job_reduced", "physical",
+  "sexual", "witness", "material", "occu_health")]), digits=5)
+
+corr_plot <- ggcorrplot(corr)
+
+
+pdf(file="~/Documents/traumaCOVID/plots/corrIVs.pdf", width=10, height=8)
+ggarrange(n_plot,
+          ggarrange(corr_plot, bar_plot, ncol = 2, labels = c("B", "C")),
+          nrow = 2,
+          labels = "A"
+          )
+dev.off()
